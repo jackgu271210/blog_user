66cccaf7aae0ab767076fdfeabe061ccd5d4c093
jackgu271210 <1098894806@.com>
2022-05-31 17:00:14 +0800

    updated

--- src/main/java/com/yanzhao/spring/boot/blog/initialzrstart/domain/User.java
+++ src/main/java/com/yanzhao/spring/boot/blog/initialzrstart/domain/User.java
@@ -1,6 +1,9 @@
 package com.yanzhao.spring.boot.blog.initialzrstart.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
@@ -17,18 +20,38 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //实体一个唯一标识
     
-    @Column(nullable = false)
+    @NotEmpty(message="姓名不能为空")
+    @Size(min=2,max=20)
+    @Column(nullable = false, length=20) //映射为字段，值不能为空
     private String name;
 
-    @Column(nullable = false)
+    @NotEmpty(message="邮箱不能为空")
+    @Size(max=50)
+    @Email(message="邮箱格式不对")
+    @Column(nullable = false, length=20,unique = true) //映射为字段，值不能为空
     private String email;
+
+    @NotEmpty(message="账号不能为空")
+    @Size(min=3,max=20)
+    @Column(nullable = false, length=20,unique = true) //映射为字段，值不能为空
+    private String username;
+
+    @NotEmpty(message="密码不能为空")
+    @Size(max=100)
+    @Column(length =100) //映射为字段，值不能为空
+    private String password;
+
+    @Column(length =200)
+    private String avatar;
 
     protected User() { //无参构造函数
     }
 
-    public User(String name, String email) {
+    public User(Long id, String name, String username, String email) {
+        this.id = id;
         this.name = name;
         this.email = email;
+        this.username = username;
     }
 
     public Long getId() {
@@ -54,11 +77,35 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
     @Override
     public String toString() {
         return String.format(
-                "User[id=%d, name='%s', age='%s']",
-                id, name, email);
+                "User[id=%d, name='%s',username='%s', age='%s']",
+                id, name, username, email);
     }
 }
--- src/main/java/com/yanzhao/spring/boot/blog/initialzrstart/repository/UserRepository.java
+++ src/main/java/com/yanzhao/spring/boot/blog/initialzrstart/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.yanzhao.spring.boot.blog.initialzrstart.repository;
 
 import com.yanzhao.spring.boot.blog.initialzrstart.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +11,19 @@ import java.util.List;
 /**
  * UserRepository接口
  */
-public interface UserRepository extends CrudRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * 根据用户姓名分页查询用户列表
+     * @param name
+     * @param pageable
+     * @return
+     */
+    Page<User> findByNameLike(String name, Pageable pageable);
+
+    /**
+     * 根据用户账号查询用户
+     * @param username
+     * @return
+     */
+    User findByUsername(String username);
+}
